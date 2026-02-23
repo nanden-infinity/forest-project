c1dc71e33b9ce3e39fc8eafda43b46c161514eba
nanden-infinity <nanden-infinity@hotmail.com>
2026-02-23 02:32:01 +0000

    hero section video add

--- src/script.js
+++ src/script.js
@@ -15,10 +15,24 @@
 //   .replace('-feira', '');
 
 // video.src = random < 25 ? `./img/video_chuva.mp4` : `./img/video_sol.mp4`;
-
+const hero = document.querySelector('.hero');
+const header = document.querySelector('header');
 const mobileButton = document.getElementById('mobile-button');
 const mobileMenu = document.getElementById('mobile-menu');
-
+const random = Math.floor(Math.random() * 10) + 20;
+const tempo = document.getElementById('tempo');
+const dia = document.getElementById('dia');
+const video = document.querySelector('#video');
+const temperatura = document.getElementById('temperatura');
+const date = new Date();
+dia.innerText = date
+  .toLocaleString('pt-pt', {
+    weekday: 'long',
+  })
+  .replace('-feira', '');
+temperatura.innerText = `${random}°`;
+tempo.innerText = random < 25 ? `🌧️ ${random + 5}%` : `🌤️`;
+video.src = random < 25 ? `./img/video_chuva.mp4` : `./img/video_sol.mp4`;
 mobileButton.addEventListener('click', () => {
   mobileMenu.classList.toggle('hidden');
   mobileMenu.classList.toggle('flex');
@@ -28,3 +42,28 @@ mobileMenu.addEventListener('click', () => {
   mobileMenu.classList.add('hidden');
   mobileMenu.classList.remove('flex');
 });
+
+// const options = {
+//   root: null,
+//   rootMargin: '-80px',
+//   threshold: 0.1,
+// };
+// const mutation = function (entries) {
+//   const [entrie] = entries;
+//   const isIntersecting = entrie.isIntersecting;
+//   if (!isIntersecting) {
+//     header.classList.add(
+//       'fixed',
+//       'w-full',
+//       'bg-verde-900',
+//       'left-0',
+//       '!top-0',
+//       '!z-99',
+//       'right-0',
+//     );
+//     console.log(header);
+//   }
+// };
+// const observer = new IntersectionObserver(mutation, options);
+
+// observer.observe(hero);
